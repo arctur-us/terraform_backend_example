be02ab253422560ddbdfc9a0f111f0e1f0621a54
ykhassen <yerbol.khassen@dataart.com>
2022-08-09 23:27:00 +0600

    add role.tf

---
+++ role.tf
@@ -0,0 +1,18 @@
+resource "aws_iam_role" "replication" {
+  name = "yk-tf-iam-role-replication-12345"
+  assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "s3.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+POLICY
+}
